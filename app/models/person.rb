e5e8f1b07b3209502482f01967ae805622b1fe05
Michael Hartl <michael@michaelhartl.com>
2008-10-23 15:14:03 -0700

    Reprepped for talk

--- app/models/person.rb
+++ app/models/person.rb
@@ -374,7 +374,15 @@ class Person < ActiveRecord::Base
 
   # Return the common contacts with the given person.
   def common_contacts_with(other_person)
-    contacts & other_person.contacts
+    sql = %(SELECT DISTINCT contact_id FROM connections
+            INNER JOIN people contact ON connections.contact_id = contact.id
+            WHERE ((person_id = ? OR person_id = ?)
+                   AND status = ? AND
+                   contact.deactivated = ? AND
+                   (contact.email_verified IS NULL
+                    OR contact.email_verified = ?)))
+    conditions = [sql, id, other_person.id, Connection::ACCEPTED, false, true]
+    Person.find(Connection.find_by_sql(conditions).map(&:contact_id))
   end
   
   protected
